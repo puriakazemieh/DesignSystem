14f115ebb8b90c0d231eeca9e0186091b94cbdcc
puriakazemieh <Puriakazemieh@gmail.com>
2024-06-11 09:54:11 +0330

    [REFACTOR] Move all IconSmallButton.kt to BaseButton.kt and clean BaseButton

--- libDesign/src/main/java/com/kazemieh/designsystem/libdesign/buttons/base/BaseButton.kt
+++ libDesign/src/main/java/com/kazemieh/designsystem/libdesign/buttons/base/BaseButton.kt
@@ -14,10 +14,7 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
-import android.view.View
-import android.view.View.OnClickListener
-import android.view.View.OnLongClickListener
-import android.view.View.OnTouchListener
+import android.view.ViewGroup
 import android.view.ViewOutlineProvider
 import android.widget.FrameLayout
 import android.widget.ProgressBar
@@ -36,7 +33,7 @@ import java.util.Arrays
 
 
 @SuppressLint("ClickableViewAccessibility", "CustomViewStyleable")
-open class BaseButton @JvmOverloads constructor(
+class BaseButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet?,
     defStyleAttr: Int = 0
@@ -44,32 +41,44 @@ open class BaseButton @JvmOverloads constructor(
     context,
     attrs,
     defStyleAttr
-), OnClickListener, OnLongClickListener, OnTouchListener {
+) {
 
     companion object {
         val TAG = "BaseButton"
     }
 
-    protected val binding: ButtonBinding
-    protected val shape: GradientDrawable
-    private val typeArray: TypedArray
+    private val inflater: LayoutInflater =
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    private var binding: ButtonBinding = ButtonBinding.inflate(inflater, this, true)
+
+    private val shape: GradientDrawable = GradientDrawable().apply {
+        shape = GradientDrawable.RECTANGLE
+    }
+    private val typeArray: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.MyButton)
+    private val threeBounce: Sprite = ThreeBounce()
 
     private lateinit var onClickListener: () -> Unit
     private lateinit var onLongClickListener: () -> Unit
     private lateinit var onTouchListener: (motionEvent: MotionEvent?) -> Boolean
 
+    private val progressBar: ProgressBar = ProgressBar(context).apply {
+        isVisible = state == StateButton.LOADING
+        binding.text.isVisible = state != StateButton.LOADING
+        binding.trailingIcon.isVisible = state != StateButton.LOADING
+        binding.leadingIcon.isVisible = state != StateButton.LOADING
+        indeterminateDrawable = threeBounce
+        threeBounce.color = tintEnableColor
+        binding.mainLayout.addView(this)
+    }
 
-//    @SuppressLint("CustomViewStyleable")
-//     var buttonTypeArray =
-//        context.obtainStyledAttributes(attrs, R.styleable.smallButton)
-
-     var state: StateButton = StateButton.ENABLE
+    var state: StateButton = StateButton.ENABLE
         set(value) {
             field = value
             state()
         }
 
-     var stateId = StateButton.ENABLE.stateId
+    private var stateId = StateButton.ENABLE.stateId
         set(value) {
             field = value
             state = when (value) {
@@ -91,13 +100,13 @@ open class BaseButton @JvmOverloads constructor(
             }
         }
 
-     var style: StyleButton = StyleButton.FIELD
+    var style: StyleButton = StyleButton.FIELD
         set(value) {
             field = value
             setStyleColor()
         }
 
-     var styleId = StyleButton.FIELD.styleId
+    private var styleId = StyleButton.FIELD.styleId
         set(value) {
             field = value
             style = when (value) {
@@ -128,13 +137,13 @@ open class BaseButton @JvmOverloads constructor(
             }
         }
 
-     var configuration: ConfigurationButton = ConfigurationButton.PRIMARY
+    var configuration: ConfigurationButton = ConfigurationButton.PRIMARY
         set(value) {
             field = value
             setStyleColor()
         }
 
-     var configurationId = ConfigurationButton.PRIMARY.configurationId
+    private var configurationId = ConfigurationButton.PRIMARY.configurationId
         set(value) {
             field = value
             configuration = if (value == ConfigurationButton.PRIMARY.configurationId) {
@@ -144,27 +153,14 @@ open class BaseButton @JvmOverloads constructor(
             }
         }
 
-     var myColors: ColorsPalletButtonImp =
-        ColorsPalletButtonImp(context, configuration, style, state)
+    var cornerRadius: Any = CornerRadius.ROUND_100
         set(value) {
             field = value
-            normalColor = myColors.normal
-            disableColor = myColors.disable
-            pressedColor = myColors.pressed
-            pressedMaskColor = myColors.pressedMask
-            tintEnableColor = myColors.tintEnable
-            tintDisableColor = myColors.tintDisable
-            borderColor = myColors.border
-            invalidateLayout()
+            if (value is CornerRadius)
+                applyCorner(value.cornerRadiusId)
+            else if (value is Int)
+                applyCorner(value)
         }
-
-    var disableColor = myColors.disable
-    var pressedColor = myColors.pressed
-    var pressedMaskColor = myColors.pressedMask
-    var tintDisableColor = myColors.tintDisable
-    var borderColor = myColors.border
-    var tintEnableColor = myColors.tintEnable
-    var normalColor = myColors.normal
 
     private var cornerRadiusId = CornerRadius.ROUND_100.cornerRadiusId
         set(value) {
@@ -184,16 +180,6 @@ open class BaseButton @JvmOverloads constructor(
             }
         }
 
-    var cornerRadius: Any = CornerRadius.ROUND_100
-        set(value) {
-            field = value
-            Log.d(TAG, "cornerRadius: $value ")
-            if (value is CornerRadius)
-                applyCorner(value.cornerRadiusId)
-            else if (value is Int)
-                applyCorner(value)
-        }
-
     var leadingIcon: Drawable? = null
         set(value) {
             field = value
@@ -206,48 +192,40 @@ open class BaseButton @JvmOverloads constructor(
             setImageDrawableTrailingIcon(value)
         }
 
-    var text: String = resources.getString(R.string.app_name)
+    var text: String? = null
         set(value) {
             field = value
-            binding.text.text = text
+            handleText()
         }
 
-    init {
+    private var myColors: ColorsPalletButtonImp =
+        ColorsPalletButtonImp(context, configuration, style, state)
+        set(value) {
+            field = value
+            setColor()
+            invalidateLayout()
+        }
 
-        val inflater: LayoutInflater =
-            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        binding = ButtonBinding.inflate(inflater, this, true)
+    private var disableColor = myColors.disable
+    private var pressedColor = myColors.pressed
+    private var pressedMaskColor = myColors.pressedMask
+    private var tintDisableColor = myColors.tintDisable
+    private var borderColor = myColors.border
+    private var tintEnableColor = myColors.tintEnable
+    private var normalColor = myColors.normal
+
+    init {
+        clickListener()
 
         binding.mainLayout.layoutDirection = LAYOUT_DIRECTION_RTL
-
         binding.text.includeFontPadding = false
-        binding.cl.setOnClickListener(this)
-        binding.cl.setOnLongClickListener(this)
-        binding.cl.setOnTouchListener(this)
-
-        shape = GradientDrawable()
-        shape.shape = GradientDrawable.RECTANGLE
-
-        typeArray = context.obtainStyledAttributes(attrs, R.styleable.MyButton)
-
-        setCornerRadius()
-        setLeadingIcon()
-        setTrailingIcon()
-        setText()
-
-        binding.text.setPaddingRelative(end = dpToPxInt(4), start = dpToPxInt(4))
-
         binding.text.setTextAppearance(R.style.FontLabelMedium)
 
 
-        binding.mainLayout.setPaddingRelative(
-            end = dpToPxInt(16),
-            start = dpToPxInt(16),
-            top = dpToPxInt(10),
-            bottom = dpToPxInt(10)
-        )
-
-
+        setText()
+        setLeadingIcon()
+        setTrailingIcon()
+        setCornerRadius()
         setState()
         setStyle()
         setConfiguration()
@@ -255,27 +233,39 @@ open class BaseButton @JvmOverloads constructor(
         typeArray.recycle()
 
 
-
-//        buttonTypeArray.recycle()
-
-
-
     }
 
-
-    private val threeBounce: Sprite? = ThreeBounce()
-    private val progressBar: ProgressBar? = ProgressBar(context).apply {
-        isVisible = state == StateButton.LOADING
-        binding.text.isVisible = state != StateButton.LOADING
-        binding.trailingIcon.isVisible = state != StateButton.LOADING
-        binding.leadingIcon.isVisible = state != StateButton.LOADING
-        indeterminateDrawable = threeBounce
-        threeBounce?.color = tintEnableColor
-        binding.mainLayout.addView(this)
+    private fun setColor() {
+        normalColor = myColors.normal
+        disableColor = myColors.disable
+        pressedColor = myColors.pressed
+        pressedMaskColor = myColors.pressedMask
+        tintEnableColor = myColors.tintEnable
+        tintDisableColor = myColors.tintDisable
+        borderColor = myColors.border
     }
 
+    private fun clickListener() {
+        binding.cl.setOnClickListener {
+            if (::onClickListener.isInitialized) {
+                onClickListener.invoke()
+            }
+        }
+        binding.cl.setOnLongClickListener {
+            if (::onLongClickListener.isInitialized) {
+                onLongClickListener.invoke()
+            }
+            true
+        }
+        binding.cl.setOnTouchListener { _, event ->
+            if (::onTouchListener.isInitialized) {
+                onTouchListener.invoke(event)
+            }
+            false
+        }
+    }
 
-    fun getStateColorTint(tintEnable: Int? = tintEnableColor): ColorStateList {
+    private fun getStateColorTint(tintEnable: Int? = tintEnableColor): ColorStateList {
         return ColorStateList(
             arrayOf(
                 intArrayOf(-android.R.attr.state_enabled),
@@ -306,31 +296,19 @@ open class BaseButton @JvmOverloads constructor(
 
     private fun setText() {
         text = typeArray.getString(R.styleable.MyButton_text)
-            ?: resources.getString(R.string.app_name)
     }
 
-    protected open fun applyCorner(value: Int) {
+    private fun applyCorner(value: Int) {
         val corner = dpToPx(value)
         shape.cornerRadii =
             floatArrayOf(corner, corner, corner, corner, corner, corner, corner, corner)
         invalidateLayout()
     }
 
-    protected fun setOnClickListener(event: () -> Unit) {
-        this.onClickListener = event
-    }
-
-    protected fun setonLongClickListener(event: () -> Unit) {
-        this.onLongClickListener = event
-    }
-
-    protected fun setonTouchListener(event: (motionEvent: MotionEvent?) -> Boolean) {
-        this.onTouchListener = event
-    }
-
-    protected fun getRippleColor(color: Int): Drawable {
+    private fun getRippleColor(color: Int): Drawable {
         val outerRadii = FloatArray(8)
-        Arrays.fill(outerRadii,
+        Arrays.fill(
+            outerRadii,
             ((cornerRadius as? CornerRadius)?.cornerRadiusId ?: (cornerRadius as? Int)
             ?: 0).toFloat()
         )
@@ -340,7 +318,7 @@ open class BaseButton @JvmOverloads constructor(
         return shapeDrawable
     }
 
-    protected fun getPressedColorSelector(): ColorStateList {
+    private fun getPressedColorSelector(): ColorStateList {
         return ColorStateList(
             arrayOf(
                 intArrayOf(android.R.attr.state_pressed),
@@ -354,8 +332,7 @@ open class BaseButton @JvmOverloads constructor(
         )
     }
 
-
-     fun setImageDrawableLeadingIcon(value: Drawable?) {
+    private fun setImageDrawableLeadingIcon(value: Drawable?) {
         binding.leadingIcon.setImageDrawable(value)
         if (value != null) {
             binding.leadingIcon.marginLayoutParams {
@@ -372,45 +349,57 @@ open class BaseButton @JvmOverloads constructor(
         }
     }
 
-     fun setImageDrawableTrailingIcon(value: Drawable?) {
-        binding.trailingIcon.setImageDrawable(value)
-        if (value != null) {
-            binding.trailingIcon.marginLayoutParams {
-                width = dpToPxInt(20)
-                height = dpToPxInt(20)
-                marginStart = dpToPxInt(2)
-            }
-        } else {
-            binding.trailingIcon.marginLayoutParams {
-                width = dpToPxInt(0)
-                height = dpToPxInt(0)
-                marginStart = dpToPxInt(0)
+    private fun setImageDrawableTrailingIcon(value: Drawable?) {
+        Log.d("949494", "setImageDrawableTrailingIcon: $text ${this.hashCode()}")
+        if (text != null) {
+            Log.d("949494", "setImageDrawableTrailingIcon: 1  $text ${this.hashCode()}")
+            binding.trailingIcon.setImageDrawable(value)
+            if (value != null) {
+                binding.trailingIcon.marginLayoutParams {
+                    width = dpToPxInt(20)
+                    height = dpToPxInt(20)
+                    marginStart = dpToPxInt(2)
+                }
+            } else {
+                binding.trailingIcon.marginLayoutParams {
+                    width = dpToPxInt(0)
+                    height = dpToPxInt(0)
+                    marginStart = dpToPxInt(0)
+                }
             }
         }
-
+//        } else {
+//            binding.trailingIcon.setImageDrawable(null)
+//        }
     }
 
-
-     open fun invalidateLayout() {
-
-        if (style != null)
-            when (style) {
-                StyleButton.TONAL -> {
-                    shape.setStroke(dpToPxInt(1), borderColor)
-                }
-
-                StyleButton.OUTLINE -> {
-                    if (state == StateButton.DISABLE) {
-                        shape.setStroke(dpToPxInt(0), borderColor)
-                    } else {
-                        shape.setStroke(dpToPxInt(1), borderColor)
-                    }
-                }
-
-                else -> {
-                    shape.setStroke(0, borderColor)
-                }
+    private fun handleText() {
+        binding.text.text = text
+        if (text != null) {
+            binding.text.isVisible = true
+            binding.text.setPaddingRelative(end = dpToPxInt(4), start = dpToPxInt(4))
+            binding.mainLayout.setPaddingRelative(
+                end = dpToPxInt(16),
+                start = dpToPxInt(16),
+                top = dpToPxInt(10),
+                bottom = dpToPxInt(10)
+            )
+        } else {
+            binding.text.setMargin(0)
+            binding.text.isVisible = false
+            binding.leadingIcon.marginLayoutParams {
+                width = dpToPxInt(24)
+                height = dpToPxInt(24)
+                marginEnd = dpToPxInt(0)
+                marginStart = dpToPxInt(0)
             }
+            binding.mainLayout.setMargin(4)
+            binding.mainLayout.setPaddingRelative(padding = 0)
+        }
+    }
+
+    private fun invalidateLayout() {
+        setStroke()
 
         if (style == StyleButton.ELEVATED) {
 
@@ -420,23 +409,24 @@ open class BaseButton @JvmOverloads constructor(
 
                 binding.mainLayout.updateLayoutParam {
                     height = dpToPxInt(40)
+                    if (text == null) width = dpToPxInt(40) else ViewGroup.LayoutParams.MATCH_PARENT
                 }
                 binding.cl.marginLayoutParams {
                     height = dpToPxInt(40)
+                    if (text == null) width = dpToPxInt(40) else ViewGroup.LayoutParams.MATCH_PARENT
                 }
             } else {
-                binding.cl.marginLayoutParams {
-                    height = dpToPxInt(36)
-                }
                 binding.mainLayout.elevation = dpToPx(2)
                 binding.mainLayout.outlineProvider = ViewOutlineProvider.BACKGROUND
                 binding.mainLayout.setMargin(2)
 
                 binding.mainLayout.updateLayoutParam {
                     height = dpToPxInt(36)
+                    if (text == null) width = dpToPxInt(36) else ViewGroup.LayoutParams.MATCH_PARENT
                 }
                 binding.cl.marginLayoutParams {
                     height = dpToPxInt(40)
+                    if (text == null) width = dpToPxInt(40) else ViewGroup.LayoutParams.MATCH_PARENT
                 }
 
             }
@@ -448,32 +438,50 @@ open class BaseButton @JvmOverloads constructor(
 
             binding.mainLayout.updateLayoutParam {
                 height = dpToPxInt(40)
+                if (text == null) width = dpToPxInt(40) else ViewGroup.LayoutParams.MATCH_PARENT
             }
             binding.cl.marginLayoutParams {
                 height = dpToPxInt(40)
+                if (text == null) width = dpToPxInt(40) else ViewGroup.LayoutParams.MATCH_PARENT
             }
         }
 
         if (state == StateButton.LOADING) {
-            binding.mainLayout.setPaddingRelative(
-                /* start =*/  dpToPxInt(0),
-                /* top =*/ dpToPxInt(0),
-                /* end =*/ dpToPxInt(0),
-                /* bottom =*/ dpToPxInt(0)
-            )
+            binding.mainLayout.setPaddingRelative(padding = 0)
         }
 
-        threeBounce?.color = tintEnableColor
+        threeBounce.color = tintEnableColor
         binding.text.setTextColor(getStateColorTint())
         binding.trailingIcon.imageTintList = getStateColorTint()
         binding.leadingIcon.imageTintList = getStateColorTint()
         binding.mainLayout.background = stateListDrawable()
 
+
+
         requestLayout()
     }
 
+    private fun setStroke() {
+        when (style) {
+            StyleButton.TONAL -> {
+                shape.setStroke(dpToPxInt(1), borderColor)
+            }
 
-     fun stateListDrawable(): StateListDrawable {
+            StyleButton.OUTLINE -> {
+                if (state == StateButton.DISABLE) {
+                    shape.setStroke(dpToPxInt(0), borderColor)
+                } else {
+                    shape.setStroke(dpToPxInt(1), borderColor)
+                }
+            }
+
+            else -> {
+                shape.setStroke(0, borderColor)
+            }
+        }
+    }
+
+    private fun stateListDrawable(): StateListDrawable {
 
         shape.setColor(normalColor)
 
@@ -516,7 +524,7 @@ open class BaseButton @JvmOverloads constructor(
         return stateListDrawable
     }
 
-     fun state() {
+    private fun state() {
         setStyleColor()
         progressBar?.isVisible = state == StateButton.LOADING
         when (state) {
@@ -575,8 +583,7 @@ open class BaseButton @JvmOverloads constructor(
         }
     }
 
-
-     fun setStyleColor() {
+    private fun setStyleColor() {
         myColors = ColorsPalletButtonImp(context, configuration, style, state)
     }
 
@@ -602,26 +609,16 @@ open class BaseButton @JvmOverloads constructor(
         )
     }
 
-
-    override fun onClick(v: View?) {
-        if (::onClickListener.isInitialized) {
-            onClickListener.invoke()
-        }
+    fun setOnClickListener(event: () -> Unit) {
+        this.onClickListener = event
     }
 
-    override fun onLongClick(v: View?): Boolean {
-        if (::onLongClickListener.isInitialized) {
-            onLongClickListener.invoke()
-        }
-        return true
+    fun setonLongClickListener(event: () -> Unit) {
+        this.onLongClickListener = event
     }
 
-    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-        if (::onTouchListener.isInitialized) {
-            return onTouchListener.invoke(event)
-        }
-        return false
+    fun setonTouchListener(event: (motionEvent: MotionEvent?) -> Boolean) {
+        this.onTouchListener = event
     }
-
 
 }
